63aa8aec7e69d131e5adb4b426ade21497e3ba0f
Tianyi Zhang <tianyi.zhang5@mail.mcgill.ca>
2020-12-06 18:39:27 -0500

    Updated models.py + admin.py

--- mcgill_website/cms/admin.py
+++ mcgill_website/cms/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Page, Event, Job
+from .models import Page, Event, Job, News
 # Register your models here.
 admin.site.register(Page)
 admin.site.register(Event)
 admin.site.register(Job)
+admin.site.register(News)
+
